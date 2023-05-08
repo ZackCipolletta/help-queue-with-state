dba3a77c03912435439dc8b8aef2e5512cd4a6ab
Zachary <ZackCipolletta@gmail.com>
2023-05-07 18:11:32 -0700

    add context consumers to implement dark/light theme

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
+import ToggleTheme from "./ToggleTheme";
 import TicketControl from "./TicketControl";
+import { ThemeContext, themes } from "./context/theme-context";
 
-function App(){
+function App() {
+  const [theme, setTheme] = useState(themes.light);
+
+  document.body.style.backgroundColor = theme.backgroundColor;
+  document.body.style.color = theme.textColor;
+
+  function toggleTheme() {
+    setTheme(theme =>
+      theme.textColor === "AntiqueWhite" ? themes.light : themes.dark
+    );
+  }
+
   return (
-    <React.Fragment>
+    <ThemeContext.Provider value={theme}>
+      {/* Notice that we've added a prop called value to our <ThemeContext.Provider> component. This is how we designate a value for our context provider. The prop must always be called value. */}
       <Header />
+      <ThemeContext.Consumer>
+        {contextTheme => <ToggleTheme theme={contextTheme} toggleTheme={toggleTheme} />}
+      </ThemeContext.Consumer>
       <TicketControl />
-    </React.Fragment>
+    </ThemeContext.Provider>
   );
 }
 
